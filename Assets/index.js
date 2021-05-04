095ce1c7d65754bc8b718cc3e34ca8619ca33812
Acacio Souza da Silva <zadfcer666@gmail.com>
2021-05-04 16:49:51 -0300

    Add files via upload

--- Assets/index.js
+++ Assets/index.js
@@ -1,4 +1,16 @@
-const imagens = ["1.jpg","1.png","blog-dia-das-maes-2019-1280x720.jpg","d5c9a79-dia-das-maes-imagem-1024x575.jpg","Dia-das-maes-2-750x452.jpg","dia-das-maes-f.png"]
+const imagens = [
+"1.jpg",
+"blog-dia-das-maes-2019-1280x720.jpg",
+"d5c9a79-dia-das-maes-imagem-1024x575.jpg",
+"Dia-das-maes-2-750x452.jpg",
+"dia-das-maes-f.png",
+"63cf50ae21a3511c9c58525ae315340e.jpg",
+"download1.jpg",
+"download2.jpg",
+"download.jpg",
+"feliz-dia-das-maes-de-fundo-com-a-familia_23-2147796625.jpg",
+"fundo_fotografico_dia_das_maes_em_tecido_591_1_20201213230024.jpg"
+]
 
 
 function typeWrite(elemento){
@@ -15,7 +27,9 @@ function typeWrite(elemento){
 const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
 
-let img = Math.floor((Math.random(1)*imagens.length));
+let img = Math.floor((Math.random(1)*(imagens.length-1)));
+console.log(imagens.length)
+console.log(imagens[img])
 const fundo = (img)=>{
     
     document.body.style.backgroundImage=`url(./Assets/${imagens[img]})` ;
